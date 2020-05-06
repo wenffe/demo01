1cd077c3f8dcae58704a744374def795425c947f
wenffe <1540925998@qq.com>
2020-05-06 20:52:03 +0800

    添加新的sout

--- src/com/itheima/demo/Demo01.java
+++ src/com/itheima/demo/Demo01.java
@@ -2,6 +2,8 @@ package com.itheima.demo;
 
 public class Demo01 {
     public static void main(String[] args) {
+        //添加新的注解
+        System.out.println("clone...");
         System.out.println("hello world");
     }
 }
